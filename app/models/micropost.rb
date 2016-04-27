37627ee376a228824f7f90815918a4e85160c0b9
akltcode <aklt.code@gmail.com>
2016-04-27 16:32:12 +0700

    Finish demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ActiveRecord::Base
+#_   attr_accessible :content, :user_id
+#_   attr_accessor :content, :user_id
+#_   belongs_to :user
+#_   validates :content, :length => { :maximum => 140 }
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+#_   attr_accessible :email, :name
+#_   attr_accessor :email, :name
+#_   has_many :microposts
+end
